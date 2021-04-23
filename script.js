8833673edf802869f32e6e64219821d057628a5d
VB2007 <balukhtin.vlad@mail.ru>
2021-04-23 18:40:48 +0700

    Script is completed

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let color='#ffffff'
+let canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    canvasWidth = canvas.clientWidth,
+    canvasHeight = canvas.clientHeight,
+    color = 'white',
+    width = 20
+canvas.width = canvasWidth
+canvas.height = canvasHeight
+
+canvas.onmousedown = () => {
+    canvas.onmousemove = (event) => {
+        context.fillStyle = color
+        context.fillRect(event.offsetX-width/2, event.offsetY-width/2, width, width)
+    }
+    canvas.onmouseup = () => {
+        canvas.onmousemove = null
+    }
+}
