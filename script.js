e2480fd6dd4cf6a1cffc24868e43031b432ef504
VB2007 <balukhtin.vlad@mail.ru>
2021-04-30 18:45:18 +0700

    Function SAVE uploaded

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ let canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
     canvasWidth = canvas.clientWidth,
     canvasHeight = canvas.clientHeight,
-    color = 'white',
-    width = 20
+    color = document.getElementById('color').value,
+    width = document.getElementById('width').value
+
 canvas.width = canvasWidth
 canvas.height = canvasHeight
+
+document.getElementById('color').oninput = function(){
+    color=this.value
+}
+document.getElementById('width').oninput = function(){
+    width=this.value
+}
+document.getElementById('img').onclick = function(){
+    let img=canvas.toDataURL('image/jpg')
+    this.href=img
+}
+
+
 
 canvas.onmousedown = () => {
     canvas.onmousemove = (event) => {
