268ac9b50a59a3f00d84b4e862612b187fcaa735
VB2007 <balukhtin.vlad@mail.ru>
2021-05-14 18:39:50 +0700

    eraser

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let canvas = document.getElementById('canvas'),
     canvasWidth = canvas.clientWidth,
     canvasHeight = canvas.clientHeight,
     color = document.getElementById('color').value,
+    background = document.getElementById("background"),
+    button = document.getElementById("tool"),
+    tool='brush',
     width = document.getElementById('width').value
 
 canvas.width = canvasWidth
 canvas.height = canvasHeight
+context.fillStyle = background.value;
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 document.getElementById('color').oninput = function(){
     color=this.value
@@ -21,14 +26,43 @@ document.getElementById('img').onclick = function(){
 
 
 
-canvas.onmousedown = () => {
+canvas.onmousedown = (e) => {
     canvas.onmousemove = (event) => {
-        context.fillStyle = color
-        context.fillRect(event.offsetX-width/2, event.offsetY-width/2, width, width)
-    }
-    context.fillStyle = color
+            if (tool == 'brush')
+                context.fillStyle = color
+            else
+                context.fillStyle = background.value
+            context.fillRect(event.offsetX - width/2, event.offsetY- width/2, width, width)
+        }
+        if (tool == 'brush')
+                context.fillStyle = color
+            else
+                context.fillStyle = background.value
+            context.fillRect(event.offsetX - width/2, event.offsetY- width/2, width, width)
     context.fillRect(event.offsetX-width/2, event.offsetY-width/2, width, width)
     canvas.onmouseup = () => {
         canvas.onmousemove = null
+    }
+}
+
+
+background.addEventListener('input', changeBG);
+
+function changeBG(){
+    context.fillStyle = background.value;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+button.addEventListener("click", changeTool);
+
+function changeTool(){
+    let btn = document.getElementById("tool");
+    if(btn.textContent == "brush"){
+        btn.textContent = "eraser";
+        tool='eraser'
+    }
+    else{
+        btn.textContent = "brush";
+        tool='brush'
     }
 }
